42a23f339dc3cb7fdbe6e842ad0844efd0edd0ac
William Colton <forfun.william.c@gmail.com>
2024-10-22 19:43:37 +0800

    卡片组件更新

--- components/card/card.js
+++ components/card/card.js
@@ -1,24 +1,9 @@
 // components/card/card.js
 Component({
-
-  /**
-   * 组件的属性列表
-   */
   properties: {
-
+    imageUrl:String,
+    title:String,
+    location:String,
+    recommendation:String
   },
-
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
 })
